fae7bfae72a1e53a4244449d07cc3ca0da3f41d4
zeroput <160378601+zeroput@users.noreply.github.com>
2024-09-27 09:57:05 +0800

    update send email script

--- send_mail_daily.py
+++ send_mail_daily.py
@@ -7,18 +7,16 @@ SENDER = os.environ.get("SENDER");
 RECEIVER = os.environ.get("RECEIVER");
 PASSWORD =  os.environ.get("PASSWORD");
 
-# 邮件发送者和接收者信息
 sender = SENDER  # 你的QQ邮箱 机密文件
 receiver = RECEIVER  # 接收者邮箱 机密文件
 
-# 邮件内容
 subject = 'Python邮件测试'  # 邮件主题
 body = '这是使用Python通过QQ邮箱发送的邮件，哈哈哈'  # 邮件正文
 
 # 设置 SMTP 服务器及端口号
 smtp_server = 'smtp.qq.com'
-smtp_port = 465  # SSL端口号
-password = 'your_authorization_code'  # QQ邮箱的授权码 机密文件
+smtp_port = 587
+password = PASSWORD  # QQ邮箱的授权码 机密文件
 
 # 创建一个MIMEText对象，指定邮件正文内容、格式和编码
 message = MIMEText(body, 'plain', 'utf-8')
@@ -28,17 +26,22 @@ message['Subject'] = Header(subject)
 
 try:
     # 连接到QQ邮箱的SMTP服务器并进行SSL加密
-    server = smtplib.SMTP_SSL(smtp_server, smtp_port)
-    
+    server = smtplib.SMTP(smtp_server, smtp_port, timeout=10)
+    # 启动 TLS 安全连接
+    server.starttls()
     # 登录QQ邮箱
     server.login(sender, password)
-    
     # 发送邮件
     server.sendmail(sender, [receiver], message.as_string())
     
     print("邮件发送成功")
-except Exception as e:
+except smtplib.SMTPException as e:
     print(f"邮件发送失败: {e}")
+except Exception as e:
+    print(f"其他错误: {e}")
 finally:
     # 关闭连接
-    server.quit()
+    try:
+        server.quit()
+    except:
+        pass
